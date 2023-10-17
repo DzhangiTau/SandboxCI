e4b6bd069215a73bb19287710c846cdd6279bf16
p.konstantinov <dzhangitau@gmail.com>
2023-10-17 17:29:02 +0400

    Another test push into master

--- app/src/main/java/com/sandbox/sandboxci/MainActivity.kt
+++ app/src/main/java/com/sandbox/sandboxci/MainActivity.kt
@@ -8,9 +8,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    override fun onResume() {
-        super.onResume()
-        println("Test Code")
-    }
 }
